df4f3a1fd4956ac47bf932ab59c0c38113ac6719
itsaimal <itsaimalyousafzai@gmail.com>
2021-05-19 13:54:21 +0500

    Collection item added as component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 
 import './App.css';
 import HomePage from "./pages/homepage/HomePage"
+import ShopPage from './pages/homepage/shop/ShopPage';
+import {Route,Switch} from "react-router-dom"
 
 function App() {
   return (
     <div className="">
-    <HomePage/>
+      <Switch>
+        <Route exact path='/' component={HomePage}/>
+        
+        <Route path='/shop' component={ShopPage}/>
+   
+  
+    </Switch>
     </div>
   );
 }
